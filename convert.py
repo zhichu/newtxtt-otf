7a001f2af4d01586180b0bd39cd6a67fa3676029
ℤhichu ℂhen <1871526+zhichu@users.noreply.github.com>
2024-02-26 19:30:34 +0800

    Add files via upload

--- convert.py
+++ convert.py
@@ -20,6 +20,7 @@ style_set_names = (
     ('English (US)', 'ss02', 'form c, slashed, narrower than capital O'),
     ('English (US)', 'ss03', 'form d, dotted, narrower than capital O'),
     ('English (US)', 'ss04', 'form e, narrower than capital O, more oblong'),
+    ('English (US)', 'ss05', 'form b, original from txtt'),
     )
 
 for g in mainfont.glyphs():
@@ -48,13 +49,23 @@ for g in mainfont.glyphs():
             mainfont.selection.select(f'{osg.glyphname}')
             mainfont.paste()
             if g.glyphname=='zero':
-                deque(map(lambda x:g.addPosSub('ss0'+str(x)+'_','zero.alt'+str(x)),range(1,5)))
+                ssg=mainfont.createChar(-1,'zero.alt5')
+                mainfont.selection.select(g.unicode)
+                mainfont.copy()
+                mainfont.selection.select(ssg.glyphname)
+                mainfont.paste()
+                mainfont.selection.select('zero.alt4')
+                mainfont.copy()
+                mainfont.selection.select(g.unicode)
+                mainfont.paste()
+                deque(map(lambda x:g.addPosSub('ss0'+str(x)+'_','zero.alt'+str(x)),range(1,6)))
 print('')
 scfont.close()
 
 #mainfont.style_set_names = style_set_names # requires the most recent version of fontforge
 
 mainfont.generate(f'newtxtt-{fontface}.otf')
+mainfont.generate(f'newtxtt-{fontface}.woff')
 mainfont.generate(f'newtxtt-{fontface}.woff2')
 mainfont.generate(f'newtxtt-{fontface}.eot')
 mainfont.generate(f'newtxtt-{fontface}.svg')
